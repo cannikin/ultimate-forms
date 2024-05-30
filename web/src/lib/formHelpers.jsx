64192397cb7129730e33660ebb35e266c7a9f9a0
Rob Cameron <cannikin@fastmail.com>
2024-05-30 15:18:26 -0700

    Updates form helpers to work with array syntax

--- web/src/lib/formHelpers.jsx
+++ web/src/lib/formHelpers.jsx
@@ -4,70 +4,109 @@ import { camelCase, titleCase } from 'change-case-all'
 
 const FormContext = createContext()
 
+// Checks if name is using array syntax, like `name[]`
+const nameIsArray = (name) => {
+  return /\[\]$/.test(name)
+}
+
 // converts the model chain and attribute name to an input element name prop
 export const modelAndAttributeName = (modelChain, name) => {
-  const modelNames = modelChain.map((m) => {
-    if (typeof m === 'object') {
-      return m.className
-    } else {
-      return m
+  if (modelChain.length) {
+    const modelNames = modelChain.map((m) => {
+      if (typeof m === 'object') {
+        return m.className
+      } else {
+        return m
+      }
+    })
+    let prop = camelCase(modelNames.shift())
+
+    for (const modelName of modelNames) {
+      prop = prop + `[${camelCase(modelName.toString())}]`
     }
-  })
-  let prop = camelCase(modelNames.shift())
 
-  for (const modelName of modelNames) {
-    prop = prop + `[${camelCase(modelName.toString())}]`
+    // check for array syntax, if present move after the square brackets:
+    // `name[]` => [name][]`
+    if (nameIsArray(name)) {
+      return `${prop}[${name.replace(/\[\]$/, '')}][]`
+    } else {
+      return `${prop}[${name}]`
+    }
+  } else {
+    return name
   }
-
-  return `${prop}[${name}]`
 }
 
 // gets the value for a given model chain and attribute name
 export const modelAndAttributeValue = (modelChain, attr) => {
-  if (typeof modelChain[modelChain.length - 1] === 'object') {
-    return modelChain[modelChain.length - 1][attr]
+  if (modelChain[0]) {
+    if (typeof modelChain[modelChain.length - 1] === 'object') {
+      return modelChain[modelChain.length - 1][attr]
+    } else {
+      return modelChain[modelChain.length - 2][attr]
+    }
   } else {
-    return modelChain[modelChain.length - 2][attr]
+    return undefined
   }
 }
 
 export const onSubmit = async (event) => {
   event.preventDefault()
 
+  // Can't use URLSearchParams to quickly parse form values because fields with
+  // the same name will override the previous value. Need to keep them separate
+  // and let the server turn them into arrays.
+  const formValues = []
+
+  // turns fields into an array of arrays, like [['name', 'Dr. John Doe']]
+  new FormData(event.target).forEach((value, name) => {
+    formValues.push([name, value])
+  })
+
+  // turns the array of arrays into a query string, like `name=Dr.+John+Doe`
+  const wwwFormUrlEncoded = formValues
+    .map(
+      ([name, value]) =>
+        `${encodeURIComponent(name)}=${encodeURIComponent(value)}`
+    )
+    .join('&')
+
   const response = await fetch(event.target.action, {
     method: event.target.method,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: new URLSearchParams(new FormData(event.target)).toString(),
+    body: wwwFormUrlEncoded,
   })
   const json = await response.json()
   console.log('response', json)
 }
 
-export const FormFor = ({
+export const FormWith = ({
   model: formModel,
-  action,
+  url,
   method = 'post',
   remote = true,
   children,
 }) => {
   return (
-    <form
-      action={action}
-      method={method}
-      onSubmit={remote ? onSubmit : undefined}
-    >
-      <FormContext.Provider value={{ model: [formModel] }}>
+    <form action={url} method={method} onSubmit={remote ? onSubmit : undefined}>
+      <FormContext.Provider value={{ model: formModel ? [formModel] : [] }}>
         {children}
       </FormContext.Provider>
     </form>
   )
 }
 
-export const FieldsFor = ({ model: fieldsModel, index, children, ...rest }) => {
+export const FieldsFor = ({
+  name,
+  model: fieldsModel,
+  index,
+  children,
+  ...rest
+}) => {
   const { model } = useContext(FormContext)
-  const modelStack = [...model, fieldsModel]
+  const modelStack = [...model, fieldsModel ? fieldsModel : { className: name }]
 
   // if the `index` prop was passed then include the index in the name so we
   // know what `id` this record has:
