484c2be394c0cdda5607fa7da668422c26067a9a
Rob Cameron <cannikin@fastmail.com>
2024-06-03 15:38:30 -0700

    Redirect from homepage to first form

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react'
+
+import { navigate, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const HomePage = () => {
+  useEffect(() => {
+    navigate(routes.simple())
+  }, [])
+
   return (
     <>
       <Metadata title="Home" description="Home page" />
